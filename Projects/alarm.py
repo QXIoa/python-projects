ec6dc79d5a778bdad8fc3bf830d23360e48c40de
QXIoa <superteddy888@gmail.com>
2024-01-28 09:56:25 -0500

    Spacing

--- Projects/alarm.py
+++ Projects/alarm.py
@@ -11,26 +11,26 @@ alarm_time = ''.join(input("Enter the time of alarm to be set in HH:MM,AM/PM for
 music_or_beep = input("Enter m for a music or b for beep sound: ")
 
 if music_or_beep=='b':
-    dur=int(input("duration in seconds: "))*1000 #winsound takes in milliseconds
+    dur = int(input("duration in seconds: "))*1000 #winsound takes in milliseconds
     freq = int(input("frequency of the noise: ")) #optimal- 500
 
-alarm_hour=alarm_time[0:2]
-alarm_minute=alarm_time[3:5]
-alarm_period=alarm_time[6:8].upper()
+alarm_hour = alarm_time[0:2]
+alarm_minute = alarm_time[3:5]
+alarm_period = alarm_time[6:8].upper()
 
-print('Setting alarm.', end="", flush=True)
+print('Setting alarm.', end = "", flush = True)
 for _ in range(random.randint(4,6)):
     wait(.58528)
-    print(".", end="", flush=True)
+    print(".", end = "", flush = True)
 
 print("\n")
 
 while True:
-    current_time=datetime.now()
-    current_hour=current_time.strftime('%I')
-    current_minute=current_time.strftime('%M')
-    current_period=current_time.strftime('%p')
-    current_date=current_time.strftime('%d')
+    current_time = datetime.now()
+    current_hour = current_time.strftime('%I')
+    current_minute = current_time.strftime('%M')
+    current_period = current_time.strftime('%p')
+    current_date = current_time.strftime('%d')
     if current_date == alarm_date and current_period == alarm_period and current_hour == alarm_hour and current_minute == alarm_minute:
         print('*'*10)
         print('| '+'Wake up!'+' |')
