a868d1ea4b9b25c7c76644ed488f7bcc2ff31edd
QXIoa <superteddy888@gmail.com>
2024-01-28 10:25:14 -0500

    Capitalization
    Co-Authored-By: QXiaol0l <157973417+qxiaol0l@users.noreply.github.com>

--- Projects/alarm.py
+++ Projects/alarm.py
@@ -10,9 +10,9 @@ alarm_date = input('Enter the date on which you want to set the alarm: ').strip(
 alarm_time = ''.join(input("Enter the time of alarm to be set in HH:MM,AM/PM format: ").split())
 music_or_beep = input("Enter m for a music or b for beep sound: ")
 
-if music_or_beep=='b':
-    dur = int(input("duration in seconds: ")) * 1000 #winsound takes in milliseconds
-    freq = int(input("frequency of the noise: ")) #optimal- 500
+if music_or_beep == 'b':
+    dur = int(input("Duration in seconds: ")) * 1000 #winsound takes in milliseconds
+    freq = int(input("Frequency of the noise: ")) #optimal- 500
 
 alarm_hour = alarm_time[0:2]
 alarm_minute = alarm_time[3:5]
